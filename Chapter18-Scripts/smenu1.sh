62f141bbb67e45caedbf45ab2f71feecf775bb9b
doushunzhi <doushunzhi@xiaomi.com>
2024-08-06 16:36:53 +0800

    fix: 优化代码按照书籍标注，更正代码内容

--- Chapter18-Scripts/smenu1.sh
+++ Chapter18-Scripts/smenu1.sh
@@ -17,8 +17,7 @@ function memusage {
 }
 
 PS3="Enter option: "
-select option in "Display disk space" "Display logged on users"¬
-"Display memory usage" "Exit program"
+select option in "Display disk space" "Display logged on users" "Display memory usage" "Exit program"
 do
    case $option in
    "Exit program")
